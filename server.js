063922ce12b6f10e54d1c742fad5f378628836cf
Shameme34 <shaikdawoodshameemahamed.uow@gmail.com>
2024-03-08 22:45:48 +0800

    step 1

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express')
+const app = express()
+const port = 3000
+
+app.set('views','./views')
+app.set('view engine','ejs')
+
+//static files
+app.use(express.static('public'))
+app.use('/css',express.static(__dirname +"public/css"))
+app.use('/js',express.static(__dirname +"public/js"))
+app.use('/img',express.static(__dirname +"public/img"))
+
+app.get('',(req,res) =>{
+    res.render('index')
+})
+
+//Listening to port 3000
+app.listen(port, () => console.info('Listening on port ',port))
