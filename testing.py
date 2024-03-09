2909a75e7d6b37fa6b47ab5601ca0d6f32b76050
Shameme34 <shaikdawoodshameemahamed.uow@gmail.com>
2024-03-09 09:04:40 +0800

    new

---
+++ testing.py
@@ -0,0 +1,27 @@
+import mysql.connector
+import json
+
+mydb = mysql.connector.connect(
+    host ='localhost',
+    user ='root',
+    password='root',
+    auth_plugin='mysql_native_password'
+)
+
+mycursor = mydb.cursor()
+mycursor.execute("use cafe22;")
+
+def GetAll():
+        try:
+            mycursor.execute("select w.date, a.cafeid, a.fullname, p.mainrole,p.job, a.mobile from useraccount a, userprofile p, workslots w where a.cafeid = p.cafeid and a.cafeid = w.cafeid;")
+            data = mycursor.fetchall()
+            numberofrow = mycursor.rowcount
+            if(numberofrow==0):
+                print("No table left")
+            else:
+                result = json.dumps(data)
+                print(result)
+        except mysql.connector.Error as error:
+            print ("Failed")
+
+GetAll()
