95256cdc306efd24726e19fc5d358dfb1b2a2e3b
Shameme34 <shaikdawoodshameemahamed.uow@gmail.com>
2024-03-10 18:22:23 +0800

    added new settings

--- server.js
+++ server.js
@@ -3,9 +3,19 @@ const path = require('path')
 const bodyParser = require("body-parser")
 var session = require('express-session')
 var flush = require('connect-flash')
+const spawn = require("child_process").spawn
 
 const app = express()
 const port = 3000
+
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+app.use(session({
+    secret:'secret',
+    resave:false,
+    saveUninitialized:false
+}))
+app.use(flush())
 
 app.set('views','./views')
 app.set('view engine','ejs')
@@ -15,6 +25,8 @@ app.use(express.static('public'))
 app.use('/css',express.static(__dirname +"public/css"))
 app.use('/js',express.static(__dirname +"public/js"))
 app.use('/img',express.static(__dirname +"public/img"))
+
+app.use(express.json())
 
 app.get('',(req,res) =>{
     res.render('index')
