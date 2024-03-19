b4f5b6be632af203534bdeb6a3c0b23f3a2f4577
Shameme34 <shaikdawoodshameemahamed.uow@gmail.com>
2024-03-19 17:42:55 +0800

    filter fixed

--- WorkShiftClass.py
+++ WorkShiftClass.py
@@ -38,7 +38,6 @@ class WorkShift:
                 print(json.dumps(result))
         except mysql.connector.Error as error:
             print ("Failed")
-
     def ManagerDeleteWorkShifts(self, id):
         try:
             mycursor.execute("delete from workshift where id = '{}'".format(id))
@@ -46,14 +45,11 @@ class WorkShift:
             print("Success")
         except mysql.connector.Error as error:
            print("Failed {}".format(error))
-    
-
     def grabWorkShiftTableColumn(self):
         mycursor.execute("select column_name from information_schema.columns where table_schema = 'FYP' and table_name = 'workshift'")
         data = mycursor.fetchall()
         result = json.dumps(data)
         print(result)
-
     def ManagerFilterWorkShift(self, selectedoption,value):
         try:
             mycursor.execute("select * from workshift where {} LIKE '%{}%';'".format(selectedoption,value))
@@ -64,9 +60,9 @@ class WorkShift:
             else:
                 result = []
                 for row in data:
-                    start_time = (datetime.min + row[3]).time().strftime('%H:%M:%S') if row[3] is not None else None
-                    end_time = (datetime.min + row[4]).time().strftime('%H:%M:%S') if row[4] is not None else None
-                    result.append((row[0], row[1], start_time, end_time, row[5]))
+                    start_time = datetime.strptime(str(row[3]), '%H:%M:%S').time().strftime('%H:%M:%S') if row[3] is not None else None
+                    end_time = datetime.strptime(str(row[4]), '%H:%M:%S').time().strftime('%H:%M:%S') if row[4] is not None else None
+                    result.append((row[0], row[1], row[2], start_time, end_time))
                 print(json.dumps(result))
         except mysql.connector.Error as error:
-            print("Failed to fetch data:", error)
+            print ("Failed")
