ea833a1d2b1de9ac75e275c756e1422eec5a0c35
Shameme34 <shaikdawoodshameemahamed.uow@gmail.com>
2024-04-22 18:24:25 +0800

    modified:   views/UpdateManagerAccount.ejs

--- testing.py
+++ testing.py
@@ -13,15 +13,10 @@ mycursor.execute("use cafe22;")
 
 def GetAll():
         try:
-            mycursor.execute("select w.date, a.cafeid, a.fullname, p.mainrole,p.job, a.mobile from userAccount a, userProfile p, workslots w where a.cafeid = p.cafeid and a.cafeid = w.cafeid;")
-            data = mycursor.fetchall()
-            numberofrow = mycursor.rowcount
-            if(numberofrow==0):
-                print("No table left")
-            else:
-                result = json.dumps(data)
-                print(result)
+            mycursor.execute("UPDATE userAccount SET {} = '{}' where employeeid = {}".format(selectedoption,value,employeeid))
+            mydb.commit()
+            print("Success")
         except mysql.connector.Error as error:
-            print ("Failed")
+            print("Failed")
 
 GetAll()
