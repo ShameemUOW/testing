5ccdb27a365fb00bdefcd81a4ca47c08cdd5ba8a
Shameme34 <shaikdawoodshameemahamed.uow@gmail.com>
2024-05-04 19:49:50 +0800

    modified:   UserProfileClass.py

--- UserProfileClass.py
+++ UserProfileClass.py
@@ -1,24 +1,21 @@
 import mysql.connector
 import json
 
-mydb = mysql.connector.connect(
-    host ='localhost',
-    user ='root',
-    password='root',
-    auth_plugin='mysql_native_password'
-)
-
-mycursor = mydb.cursor()
-mycursor.execute("use FYP;")
-
 class UserProfile:
     def __init__(self):
-        pass
+        self.mydb = mysql.connector.connect(
+            host ='bdpspl67hpsxmkiiukdu-mysql.services.clever-cloud.com',
+            user ='u5fgsonwyoke5bff',
+            password='nHsZUdEJQ30AYtYXN6nF',
+            database='bdpspl67hpsxmkiiukdu',
+            port = '3306'
+        )
+        self.mycursor = self.mydb.cursor()
     def UserProfileSelect(self):
         try:
-            mycursor.execute("select distinct mainrole From userprofile;")
-            searchingdata = mycursor.fetchall()
-            numberofrow = mycursor.rowcount
+            self.mycursor.execute("select distinct mainrole From userProfile;")
+            searchingdata = self.mycursor.fetchall()
+            numberofrow = self.mycursor.rowcount
             if(numberofrow==0):
                 print("No table left")
             else:
@@ -28,33 +25,33 @@ class UserProfile:
             print ("Failed")
     def createUserProfile(self,employeeid,selectedoption,role):
         try:
-            mycursor.execute("INSERT INTO  userprofile VALUES ('{}','{}', '{}')".format(employeeid,selectedoption,role))
-            mydb.commit()
+            self.mycursor.execute("INSERT INTO  userProfile VALUES ('{}','{}', '{}')".format(employeeid,selectedoption,role))
+            self.mydb.commit()
             print("Success")
         except mysql.connector.Error as error:
             print("Failed")
     def updateUserProfile(self,employeeid,selectedoption,role):
         if (selectedoption == "Profile"):
             try:
-                mycursor.execute("update userprofile SET mainrole = '{}' where employeeid = '{}'".format(role,employeeid))
-                mydb.commit()
-                mycursor.execute("update useraccount SET placeholder = '{}' where employeeid = '{}'".format(role,employeeid))
-                mydb.commit()
+                self.mycursor.execute("update userProfile SET mainrole = '{}' where employeeid = '{}'".format(role,employeeid))
+                self.mydb.commit()
+                self.mycursor.execute("update userAccount SET placeholder = '{}' where employeeid = '{}'".format(role,employeeid))
+                self.mydb.commit()
                 print("Success")
             except mysql.connector.Error as error:
                 print("Failed")
         elif (selectedoption == "Role"):
             try:
-                mycursor.execute("update userprofile SET job = '{}' where employeeid = '{}'".format(role,employeeid))
-                mydb.commit()
+                self.mycursor.execute("update userProfile SET job = '{}' where employeeid = '{}'".format(role,employeeid))
+                self.mydb.commit()
                 print("Success")
             except mysql.connector.Error as error:
                 print("Failed")
     def AdminViewUserProfile(self):
         try:
-            mycursor.execute("select employeeid, mainrole, job from userprofile;")
-            data = mycursor.fetchall()
-            numberofrow = mycursor.rowcount
+            self.mycursor.execute("select employeeid, mainrole, job from userProfile;")
+            data = self.mycursor.fetchall()
+            numberofrow = self.mycursor.rowcount
             if(numberofrow==0):
                 print("No table left")
             else:
